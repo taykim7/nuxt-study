2a069d886c0cd6df1190bc39422d798b3f3370dd
taykim7 <th_77@naver.com>
2024-04-01 18:56:13 +0900

    asyncData 실험
    - asyncData는 페이지 컴포넌트에서만 사용이 가능하다
    - asyncData는 페이지 진입 전에 호출되는 속성이라 내부에서 생성된 변수는 return 해야 사용가능하다.

---
+++ components/ProductList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{ products }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  // asyncData속성은 pages 컴포넌트에 위치해야한다. (pageds 폴더 안에 있는 컴포넌트)
+  async asyncData() {
+    const response = await axios.get('http://localhost:3000/products');
+    const products = response.data;
+    return { products };
+  },
+}
+</script>
+
+<style>
+
+</style>
--- pages/main.vue
+++ pages/main.vue
@@ -1,13 +1,30 @@
 <template>
   <div>
-    <h1>메인페이지</h1>
-    <p></p>
+    <p>메인페이지</p>
+    <ProductList :products="products"></ProductList>
   </div>
 </template>
 
-<script>
-export default {
+<script> 
+// import axios from 'axios';
+import ProductList from '~/components/ProductList.vue';
 
+export default {
+  components: { ProductList },
+  // async asyncData() {
+  //   const response = await axios.get('http://localhost:3000/products');
+  //   const products = response.data;
+  //   return { products };
+  // },
+  // data() {
+  //   return {
+  //     products: [],
+  //   }
+  // },
+  // async created() {
+  //   const response = await axios.get('http://localhost:3000/products');
+  //   this.products = response.data;
+  // } //==> 깜빡이는 문제가 발생 -> asyncData 를 사용하자
 }
 </script>
 
