294cdf70d22e8dc690b1ce5045ea8c17e57e8783
taykim7 <th_77@naver.com>
2024-04-02 18:33:50 +0900

    구성 변경, style 추가

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,8 @@
 <template>
   <div>
     <header>
-      <h1>{{ $route.name }} 페이지</h1>
-      <nuxt-link to="/">홈페이지</nuxt-link>
-      <nuxt-link to="/main">메인</nuxt-link>
-      <nuxt-link to="/product">상품</nuxt-link>
+      <h1>넉스트를 배워보자</h1>
+
     </header>
     <Nuxt />
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,77 @@
 <template>
-  <div>
-    <h1>홈페이지</h1>
-    <p>nuxt를 배워보자</p>
+  <div class="app">
+    <div>
+      <input type="text" />
+    </div>
+    <main>
+      <ul>
+        <li v-for="product in products"
+        :key="product.id"
+        class="item flex"
+        @click="moveToDetailPage(product.id)">
+          <img class="product-image" :src="product.imageUrl" :alt="product.name" />
+          <p>{{ product.name }}</p>
+          <span>{{ product.price }}</span>
+        </li>
+      </ul>
+    </main>
   </div>
 </template>
 
-<script>
-export default {
+<script> 
+import axios from 'axios';
 
+export default {
+  async asyncData() {
+    // 이미지 api 종료되서 교체
+    const response = await axios.get('http://localhost:3000/products');
+    const products = response.data.map(item => ({
+      ...item,
+      imageUrl: `https://picsum.photos/id/${Math.floor(
+      Math.random() * 30
+    )}/640/480`,
+    }));
+    return { products };
+  },
+
+  methods: {
+    moveToDetailPage(id) {
+      console.log(id)
+    }
+  }
 }
 </script>
+
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+}
+.item {
+  display: inline-block;
+  width: 400px;
+  height: 300px;
+  text-align: center;
+  margin: 0 0.5rem;
+  cursor: pointer;
+}
+.product-image {
+  width: 400px;
+  height: 250px;
+}
+.app {
+  position: relative;
+}
+.cart-wrapper {
+  position: sticky;
+  float: right;
+  bottom: 50px;
+  right: 50px;
+}
+.cart-wrapper .btn {
+  display: inline-block;
+  height: 40px;
+  font-size: 1rem;
+  font-weight: 500;
+}
+</style>
