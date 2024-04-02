cb74ef3df144b7c5ea8cf029e3bf157d5235d116
taykim7 <th_77@naver.com>
2024-04-02 19:15:39 +0900

    동적라우팅 파일에서 asyncData속성의 파라미터인 context속성으로 파라미터 가져오기

--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -1,18 +1,55 @@
 <template>
   <div>
-    <h1>상세 페이지</h1>
+    <div class="container">
+      <div class="main-panel">
+        <img
+          class="product-image"
+          :src="product.imageUrl"
+          :alt="product.name"
+        />
+      </div>
+      <div class="side-panel">
+        <p class="name">{{ product.name }}</p>
+        <p class="price">{{ product.price }}</p>
+        <!-- <button type="button" @click="addToCart">Add to Cart</button> -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+// 언더바를 붙힌 파일은 동적라우트로 정의된다.
+import { fetchProductById } from '@/api/index'
+
 export default {
-  // 언더바를 붙힌 파일은 동적라우트로 정의된다.
-  created() {
-    console.log(this.$route);
-  }
+  async asyncData({ params }) {
+    const response = await fetchProductById(params.id);
+    const product = response.data;
+    // 이미지 api 교체
+    product.imageUrl = `https://picsum.photos/id/${Math.floor(
+      Math.random() * 30
+    )}/640/480`
+    return { product }
+  },
 }
 </script>
 
-<style>
-
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0;
+}
+.product-image {
+  width: 500px;
+  height: 375px;
+}
+.side-panel {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 220px;
+  text-align: center;
+  padding: 0 1rem;
+}
 </style>
