3e2ee902ff0b77a57ede8df0c6ec817a3b42a3e0
taykim7 <th_77@naver.com>
2024-04-04 19:54:26 +0900

    카트 css

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
-    카트입니다.
+  <div class="container">
+    <h1 class="list-title">cart page</h1>
+    <div class="list-wrapper">
+      <ul>
+        <li v-for="cartItem in $store.state.cartItems"
+          :key="cartItem.id"
+          class="list-item"
+        >
+          <img class="thumbnail"
+            :src="cartItem.imageUrl"
+            :alt="cartItem.name">
+          <div class="description">
+            <p>{{ cartItem.name }}</p>
+            <span>{{ cartItem.price }}</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="extra-panel">
+      <button>구매하기</button>
+    </div>
+
   </div>
 </template>
 
@@ -11,6 +31,29 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.container {
+  margin: 2rem 10rem;
+}
+.list-title {
+  font-weight: 700;
+  font-size: 1.4rem;
+}
+.list-wrapper {
+  margin: 0.4rem 0;
+}
+.list-item {
+  display: flex;
+}
+.thumbnail {
+  width: 100px;
+  height: 100px;
+}
+.description {
+  padding: 2rem 1rem;
+}
+.extra-panel {
+  text-align: right;
+  padding: 0.2rem 0;
+}
 </style>
