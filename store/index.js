0525be322a1b638ec5bd08219ced04dcb6efa5c7
taykim7 <th_77@naver.com>
2024-04-08 19:21:54 +0900

    카트 조회 store 등록
    - 상수화

--- store/index.js
+++ store/index.js
@@ -1,5 +1,13 @@
 // store/index.js -- 파일 생성하니 알아서 vuex 다운받음ㄷㄷ
 
+import { fetchCartItems } from "~/api";
+
+// constants
+// 상수화
+export const FETCH_CART_ITEMS = ' FETCH_CART_ITEMS';
+
+
+// store
 export const state = () => ({
   cartItems: [],
 })
@@ -8,4 +16,15 @@ export const mutations = {
   addCartItem(state, cartItems) {
     state.cartItems.push(cartItems);
   },
+  setCartItems(state, cartItems) {
+    state.cartItems = cartItems;
+  }
+}
+
+export const actions = {
+  // 동적으로 키값 정의
+  async [FETCH_CART_ITEMS]({ commit }) {
+    const { data } = await fetchCartItems();
+    commit('setCartItems', data);
+  }
 }
